4dda91211a0484cfb32ba86f60667ceb2a8171c6
Muhammad Sahrul Ikhsan <sahrulikhsan44@gmail.com>
2024-07-14 14:22:29 +0700

    penerapan dasar socket programing

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import socket
 import threading
 import time
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+HOST = "192.168.1.18"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 
